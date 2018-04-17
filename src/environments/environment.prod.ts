67c01ee9e8e28acfa7682fdae7f19669661252ce
ROOT <ROOT@ROOT>
2018-04-17 23:09:58 +0800

    ng-organic-shop first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCErJBLE9HMSc9fAYXNYcdDG1l2B_gAI8w",
+    authDomain: "oshop-87882.firebaseapp.com",
+    databaseURL: "https://oshop-87882.firebaseio.com",
+    projectId: "oshop-87882",
+    storageBucket: "oshop-87882.appspot.com",
+    messagingSenderId: "984213142946"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCErJBLE9HMSc9fAYXNYcdDG1l2B_gAI8w",
+    authDomain: "oshop-87882.firebaseapp.com",
+    databaseURL: "https://oshop-87882.firebaseio.com",
+    projectId: "oshop-87882",
+    storageBucket: "oshop-87882.appspot.com",
+    messagingSenderId: "984213142946"
+  }
+};
